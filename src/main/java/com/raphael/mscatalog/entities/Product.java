c4deaae777ebbaf3823b3a6815217ca8f3cbccd4
Raphael Araújo <raphaelaraujotoc@outlook.com>
2024-11-07 17:09:16 -0300

    Criando entidade Product

---
+++ src/main/java/com/raphael/mscatalog/entities/Product.java
@@ -0,0 +1,48 @@
+package com.raphael.mscatalog.entities;
+
+import com.raphael.mscatalog.dto.ProductCreateDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+
+    public Product(ProductCreateDTO productCreateDTO) {
+        this.name = productCreateDTO.getName();
+        this.description = productCreateDTO.getDescription();
+        this.price = productCreateDTO.getPrice();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/raphael/mscatalog/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.raphael.mscatalog.repositories;
+
+import com.raphael.mscatalog.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
